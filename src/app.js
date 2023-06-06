bb1af38e67ccd8f533c5ff1e7fbbab7f99f4dd6d
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-06-06 15:26:18 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,58 +10,55 @@ const emailUser = document.querySelector("#email-user");
 const validateEmail = () => {
     const pattern = emailInput.pattern;
     const rexPattern = new RegExp(pattern, "gi");
-    let isValid = false;
-    if (!rexPattern.test(emailInput.value)) {
-        emailNotValid()
-    } else {
-        emailValid();
-        isValid = true;
-    }
+    const isValid = rexPattern.test(emailInput.value);
 
     if (isValid) {
+        emailValid();
         displayThanks();
+    } else {
+        emailNotValid();
     }
-}
+};
 
 const emailNotValid = () => {
-    if (!form.querySelector("strong")) {
+    if (!form.contains(errorMessage)) {
         createError();
         emailInput.classList.add("border-primary-tomato", "text-primary-tomato", "bg-red-200");
     }
 };
 
 const emailValid = () => {
-    if (form.querySelector("strong")) {
+    if (form.contains(errorMessage)) {
         errorMessage.remove();
         emailInput.classList.remove("border-primary-tomato", "text-primary-tomato", "bg-red-200");
     }
-}
+};
 
 const createError = () => {
     errorMessage.textContent = "Valid email required";
     errorMessage.setAttribute('class', 'absolute right-0 top-0 text-xs text-primary-tomato');
     errorMessage.setAttribute('style', 'top: 5px ; right: 0')
     form.appendChild(errorMessage);
-}
+};
 
 const displayThanks = () => {
     thankCard.classList.remove('hidden');
     mainCard.classList.add('hidden');
     emailUser.textContent = emailInput.value;
-}
+};
 
 submitButton.addEventListener('click', e => {
-    validateEmail();
     e.preventDefault();
+    validateEmail();
 });
 
 emailInput.addEventListener('keypress', e => {
     if (e.event === 'Enter') {
         submitButton.click();
     }
-})
+});
 
 dismissButton.addEventListener('click', () => {
     thankCard.classList.add('hidden');
     mainCard.classList.remove('hidden');
-})
+});
